7b917f252e3db2544182b8a9074fbd7e0edd87b9
shifter <muislarques@gmail.com>
2025-04-12 21:55:48 +0100

    reverse polish notation.

---
+++ problems/150_reverse-polish-notation.py
@@ -0,0 +1,32 @@
+# 150. Evaluate Reverse Polish Notation
+class Solution:
+  def evalRPN(self, tokens):
+    numStack = []
+
+    for val in tokens:
+      if val not in {"+","-","*","/"}:
+        numStack.append(int(val))
+      else:
+        temp2 = numStack.pop()
+        temp1 = numStack.pop()
+
+        if val == "+":
+          numStack.append(temp1 + temp2)
+          print(temp1, temp2, val)
+        elif val == "-":
+          numStack.append(temp1 - temp2)
+          print(temp1, temp2, val)
+        elif val == "*":
+          numStack.append(temp1 * temp2)
+          print(temp1, temp2, val)
+        elif val == "/":
+          numStack.append(int(temp1 / temp2))
+          print(temp1, temp2, val)
+
+    return numStack[0]
+
+solution = Solution()
+# solution.evalRPN(["2","1","+","3","*"])
+# print(solution.evalRPN(["2","1","+","3","*"]))
+# solution.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"])
+print(solution.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
