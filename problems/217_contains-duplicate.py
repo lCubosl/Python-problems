70883ba124143a5fdf9f14c1c519ab839d99fbef
shifter <muislarques@gmail.com>
2025-06-28 01:41:45 +0100

    set solution for 217_contains_duplicate

--- problems/217_contains-duplicate.py
+++ problems/217_contains-duplicate.py
@@ -16,24 +16,6 @@ print("Expected: false, Output: ", solution.containsDuplicate2(nums = [1,2,3,4])
 print("Expected: true, Output: ", solution.containsDuplicate2(nums = [1,1,1,3,3,4,3,2,4,2]))
 print("---------------------------------------")
 
-# hashset
-class Solution():
-  def containsDuplicate3(self, nums):
-    seen = set()
-
-    for n in nums:
-      if n in seen:
-        return True
-      seen.add(n)
-        
-    return False
-
-solution = Solution()
-print("Expected: true, Output: ", solution.containsDuplicate3(nums = [1,2,3,1]))
-print("Expected: false, Output: ", solution.containsDuplicate3(nums = [1,2,3,4]))
-print("Expected: true, Output: ", solution.containsDuplicate3(nums = [1,1,1,3,3,4,3,2,4,2]))
-print("---------------------------------------")
-
 # set length
 class Solution():
   def containsDuplicate4(self, nums):
@@ -43,4 +25,22 @@ solution = Solution()
 print("Expected: true, Output: ", solution.containsDuplicate4(nums = [1,2,3,1]))
 print("Expected: false, Output: ", solution.containsDuplicate4(nums = [1,2,3,4]))
 print("Expected: true, Output: ", solution.containsDuplicate4(nums = [1,1,1,3,3,4,3,2,4,2]))
+print("---------------------------------------")
+
+# hashset
+class Solution():
+  def containsDuplicate3(self, nums):
+    seen = set()
+
+    for num in nums:
+      if num in seen:
+        return True      
+      seen.add(num)
+    
+    return  False
+  
+solution = Solution()
+print("Expected: true, Output: ", solution.containsDuplicate3(nums = [1,2,3,1]))
+print("Expected: false, Output: ", solution.containsDuplicate3(nums = [1,2,3,4]))
+print("Expected: true, Output: ", solution.containsDuplicate3(nums = [1,1,1,3,3,4,3,2,4,2]))
 print("---------------------------------------")
