fd67a6241b5c0708ac050e67ded42f85d3f171a3
shifter <muislarques@gmail.com>
2025-07-24 00:09:16 +0100

    83.remove-duplicate-sorted-list.py (no solution yet)

---
+++ problems/83.Remove-duplicates-sorted-list.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+  def deleteDuplicates(self, head):
+    pass
+
+# 1,1,2
+# 1,1,2,3,3
