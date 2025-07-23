f8cc8f17d885fcbf17df6a654edebcc006d04500
shifter <muislarques@gmail.com>
2025-07-23 07:44:59 +0100

    9_palindrome l,r pointer

--- problems/9_palindrome_number.py
+++ problems/9_palindrome_number.py
@@ -6,6 +6,18 @@ class Solution(object):
     if number_arr == reversed_arr:
       return True
     return False
+  
+  def isPalindrome(self, x):
+    number_arr = [n for n in str(x)]
+    l, r = 0, len(number_arr) - 1
+
+    while l < r:
+      if number_arr[l] != number_arr[r]:
+        return False
+      l += 1
+      r -= 1
+
+    return True
 
 solution = Solution()
 print(solution.isPalindrome(x = 121 ))
