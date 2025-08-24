76f734d95d48d31504850894115945a7f668a5c4
shifter <muislarques@gmail.com>
2025-08-24 02:15:47 +0100

    203_remo-lisnkedlist solved O(n)

--- problems/203_remove-linked-list-elements.py
+++ problems/203_remove-linked-list-elements.py
@@ -7,14 +7,31 @@ class Node(object):
 def printList(head):
   current = head
   while current:
-    print(current.data, end=" -> ")
+    print(current.val, end=" -> ")
     current = current.next
   print("None")
 
 def removeElements(head, val):
-    """
-    :type head: Optional[ListNode]
-    :type val: int
-    :rtype: Optional[ListNode]
-    """
-        
+  dummy = Node(0)
+  dummy.next = head
+  current = dummy
+
+  while current.next:
+    if current.next.val == val:
+      current.next = current.next.next
+    else:
+      current = current.next
+
+  return dummy.next
+
+if __name__ == "__main__":
+  # Create list: 1 -> 2 -> 6 -> 3 -> 4 -> 5 -> 6
+  head = Node(1, Node(2, Node(6, Node(3, Node(4, Node(5, Node(6)))))))
+
+  print("Original list:")
+  printList(head)
+
+  new_head = removeElements(head, 6)
+
+  print("After removing 6:")
+  printList(new_head)
