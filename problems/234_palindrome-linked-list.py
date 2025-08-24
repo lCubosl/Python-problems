61df3d65a2b66b15b0117a749c61896eafefb7cd
shifter <muislarques@gmail.com>
2025-08-25 00:05:11 +0100

    234_palindrom-linked-list using dictionary and l,r pointers

--- problems/234_palindrome-linked-list.py
+++ problems/234_palindrome-linked-list.py
@@ -1,5 +1,5 @@
 # Definition for singly-linked list.
-class ListNode(object):
+class Node(object):
   def __init__(self, val=0, next=None):
     self.val = val
     self.next = next
@@ -11,5 +11,26 @@ def printList(head):
     current = current.next
   print("None")
 
-def isPalindrome(self, head):
-  pass
+def isPalindrome(head):
+  nums = []
+
+  while head:
+    nums.append(head.val)
+    head = head.next
+  
+  l,r = 0, len(nums) - 1
+  while l <= r:
+    if nums[l] != nums[r]:
+      return False
+    l += 1
+    r -= 1
+  return True
+
+if __name__ == "__main__":
+  head = Node(1, Node(2, Node(2, Node(2))))
+
+  print("Original list:")
+  printList(head)
+
+  result = isPalindrome(head)
+  print(result)
