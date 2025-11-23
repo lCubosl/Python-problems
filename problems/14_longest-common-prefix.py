84ec7ba844015e05c643d9417e9a059496e0f425
shifter <muislarques@gmail.com>
2025-11-23 20:38:41 +0000

    comments and debugging

--- problems/14_longest-common-prefix.py
+++ problems/14_longest-common-prefix.py
@@ -14,17 +14,17 @@ class Solution(object):
         
         return prefix
     
-    def longestCommonPrefix(self,strs):
-        if not strs:
-            return ""
+    # def longestCommonPrefix(self,strs):
+    #     if not strs:
+    #         return ""
         
-        for i in range(len(strs[0])):
-            ch = strs[0][i]
-            for s in strs[1:]:
-                if i == len(s) or s[i] != ch:
-                    return strs[0][:i]
+    #     for i in range(len(strs[0])):
+    #         ch = strs[0][i]
+    #         for s in strs[1:]:
+    #             if i == len(s) or s[i] != ch:
+    #                 return strs[0][:i]
 
-            return strs
+    #         return strs
 
 solution = Solution()
 print(solution.longestCommonPrefix(["flower","flow","flight"]))
