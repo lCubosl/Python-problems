efe233fd366e1041b3de092e0ef9eb572e0c6f38
shifter <muislarques@gmail.com>
2026-01-14 01:45:33 +0000

    58

--- problems/58_length-last-word.py
+++ problems/58_length-last-word.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution1(object):
     def lengthOfLastWord(self, s):
         i = len(s) - 1
         length = 0
@@ -11,7 +11,22 @@ class Solution(object):
             i -= 1
         
         return length
+    
+class Solution2(object):
+    def lengthOfLastWord(self, s):
+        words = s.split()
+        return len(words[-1])
 
-s = Solution()
+class Solution3(object):
+    def lengthOfLastWord(self, s):
+        count = 0
+        for ch in reversed(s):
+            if ch != ' ':
+                count += 1
+            elif count > 0:
+                break
+        return count
+
+s = Solution1()
 s.lengthOfLastWord("string word")
 print(s.lengthOfLastWord("string word"))
