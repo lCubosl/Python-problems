f37bfbe2f5d4a64aea23649a4e2af7b351eadfb8
shifter <muislarques@gmail.com>
2026-01-15 01:33:32 +0000

    28

---
+++ problems/28_find-index-occurence.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        if needle == "":
+            return 0
+        
+        n,m = len(haystack), len(needle)
+
+        for i in range(n-m+1):
+            if haystack[i:i+m] == needle:
+                return i
+        
+        return -1
+
+s = Solution()
+s.strStr("sadbutsad", "sad")        
+print(s.strStr("sadbutsad", "sad"))
