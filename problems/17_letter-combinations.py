02328a816bc517f5b5ed8e65037445c6f28e1864
LuisM <muislarques@gmail.com>
2026-01-28 01:31:11 +0000

    17

---
+++ problems/17_letter-combinations.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        if not digits:
+            return []
+
+        phone = {
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': "tuv",
+            '9': "wxyz"
+        }
+
+        result = []
+
+        def backtrack(index, path):
+            # Base case: full combination built
+            if index == len(digits):
+                result.append(path)
+                return
+
+            letters = phone[digits[index]]
+            for ch in letters:
+                backtrack(index + 1, path + ch)
+
+        backtrack(0, "")
+        return result
+
+s = Solution()
+
+print(s.letterCombinations("23"))
+print(s.letterCombinations("2"))
+print(s.letterCombinations(""))
